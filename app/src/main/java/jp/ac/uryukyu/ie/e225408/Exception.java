757af2ce4bf17c06073ed0a1ede30bba41138a7a
Kenmochi Hiroki <e225408@ie.u-ryukyu.ac.jp>
2023-12-11 23:53:32 +0900

    java. fix

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e225408/Exception.java
@@ -0,0 +1,9 @@
+package jp.ac.uryukyu.ie.e225408;
+
+public class Exception {
+    public static void main(String[] args) {
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
+        System.out.println(value);
+}
+}
