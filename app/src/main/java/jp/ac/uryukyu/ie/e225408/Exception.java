456db33baaef615b6e50c13de89bbe904c330c0a
e225408 <150403632+e225408@users.noreply.github.com>
2024-02-03 22:00:14 +0900

    Delete app/src/main/java/jp/ac/uryukyu/ie/e225408 directory

--- app/src/main/java/jp/ac/uryukyu/ie/e225408/Exception.java
+++
@@ -1,9 +0,0 @@
-package jp.ac.uryukyu.ie.e225408;
-
-public class Exception {
-    public static void main(String[] args) {
-        String str = "壱百満";
-        int value = Integer.parseInt(str);
-        System.out.println(value);
-}
-}
